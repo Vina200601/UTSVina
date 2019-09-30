4d719cc8055bb863c032e803616ed8e15020f62e
Nadialestari24 <nadialestari0107@gmail.com>
2019-09-30 14:36:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utsvina/Sayuran.java
@@ -0,0 +1,42 @@
+package com.example.utsvina;
+
+import java.util.List;
+
+public class Sayuran  {
+    private String sayuran;
+    private String latin;
+    private List<String>vitamin;
+    private String gambar;
+
+    public String getSayuran() {
+        return sayuran;
+    }
+
+    public void setSayuran(String sayuran) {
+        this.sayuran = sayuran;
+    }
+
+    public String getLatin() {
+        return latin;
+    }
+
+    public void setLatin(String latin) {
+        this.latin = latin;
+    }
+
+    public List<String> getVitamin() {
+        return vitamin;
+    }
+
+    public void setVitamin(List<String> vitamin) {
+        this.vitamin = vitamin;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+}
